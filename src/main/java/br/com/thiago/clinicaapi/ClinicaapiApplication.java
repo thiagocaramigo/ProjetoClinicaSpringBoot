1ff12097ad4caee1849091fced150dae3ced9ff4
Thiago Caramigo <thiagodazuera@gmail.com>
2021-10-20 08:51:16 -0300

    Inicio do Pojeto Clinica API

---
+++ src/main/java/br/com/thiago/clinicaapi/ClinicaapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiago.clinicaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/thiago/clinicaapi/ClinicaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiago.clinicaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
