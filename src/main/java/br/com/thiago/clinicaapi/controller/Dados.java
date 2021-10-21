c48d336a5a91f7fc2f51fbabc1036b5c864faf43
Thiago Caramigo <thiagodazuera@gmail.com>
2021-10-21 11:48:09 -0300

    Instalação do JPA|Implementação do Repository

--- src/main/java/br/com/thiago/clinicaapi/controller/Dados.java
+++ src/main/java/br/com/thiago/clinicaapi/controller/Dados.java
@@ -3,14 +3,17 @@ package br.com.thiago.clinicaapi.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.thiago.clinicaapi.model.Cliente;
+import br.com.thiago.clinicaapi.repository.DadosRepository;
 
 
 /*
@@ -22,58 +25,21 @@ import br.com.thiago.clinicaapi.model.Cliente;
  */
 
 
-
-
-
-
-
 @RestController
 @RequestMapping("/")
-public class Dados {	
-
-	@GetMapping
-	public String inicial() {
-	    return"Olá Você está na página inicial do projeto";
+public class Dados {
 	
+	@Autowired
+	private DadosRepository dr;
+	
+	@PostMapping("/cad")
+	public String cadastro(@RequestBody Cliente cliente) {
+		dr.save(cliente);
+		return "Cliente cadastrado com sucesso";
+	}
+	
+	@GetMapping("/clientes")
+	public List<Cliente> clientes(){
+		return dr.findAll();
+	}
 }
-	
-     @GetMapping("/cliente")	
-	 public String cliente() {
-		 return "Adilson Peçanha";
-	 }
-     
-     @GetMapping("/clientes")
-	 public List<Cliente> clientes() {
-    	 List<Cliente> lst = new ArrayList<Cliente>();
-    	 Cliente c1 = new Cliente();
-    	 Cliente c2 = new Cliente();
-    	 
-    	 c1.setNome("Wagner");
-    	 c2.setNome("Vanessa");
-    	 
-    	 lst.add(c1);
-    	 lst.add(c2);
-    	 
-          return lst;
-     }
-     @PostMapping("/cadastro")  
-     public String cadastro() {
-    	 return "Cadastro Realizado";
-    	 
-     }
-     @PutMapping("/atualizar")
-     public String atualizar() {
-    	 return "Dados atualizados";
-     }
-     @DeleteMapping("/apagar")
-     public String apagar() {
-    	 return "Dados apagados";
-     }
-
-
-}	
-
-	
-
-     
-     
--- src/main/java/br/com/thiago/clinicaapi/model/Cliente.java
+++ src/main/java/br/com/thiago/clinicaapi/model/Cliente.java
@@ -1,34 +1,90 @@
 package br.com.thiago.clinicaapi.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
+	
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 private Long id;
+	 
+     
+     public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "nomeCliente",length = 30, nullable = false)
      private String nome;
+     
+     @Column(nullable = false)
      private Integer idade;
+     
+     @Column(length = 100)
      private String endereço;
+     
+     @Column
      private String telefone;
+
+	
+     
+     
+     
+     
+    public Cliente() {
+	}
+    
+    
+
+	public Cliente(String nome, Integer idade, String endereço, String telefone) {
+		this.nome = nome;
+		this.idade = idade;
+		this.endereço = endereço;
+		this.telefone = telefone;
+	}
+
+
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public Integer getIdade() {
 		return idade;
 	}
+
 	public void setIdade(Integer idade) {
 		this.idade = idade;
 	}
+
 	public String getEndereço() {
 		return endereço;
 	}
+
 	public void setEndereço(String endereço) {
 		this.endereço = endereço;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-     
      
 }
